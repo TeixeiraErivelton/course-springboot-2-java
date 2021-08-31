6a99a9681fb4f9cdf91028d5833c65b7514709be
Erivelton Teixeira <67931546+TeixeiraErivelton@users.noreply.github.com>
2021-08-31 16:24:33 -0300

    Category

--- src/main/java/com/erivelton/Course/config/TestConfig.java
+++ src/main/java/com/erivelton/Course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.erivelton.Course.config;
 
+import com.erivelton.Course.entities.Category;
 import com.erivelton.Course.entities.Order;
 import com.erivelton.Course.entities.User;
 import com.erivelton.Course.entities.enums.OrderStatus;
+import com.erivelton.Course.repositories.CategoryRepository;
 import com.erivelton.Course.repositories.OrderRepository;
 import com.erivelton.Course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,10 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+
 
     @Override
     public void run(String... args) throws Exception {
@@ -33,8 +39,13 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT,u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u1);
 
+        Category cat1 = new Category(null, "Eletronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
     }
 }
